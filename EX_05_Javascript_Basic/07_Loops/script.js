947f76f2009ab9f9e80980e8d54eeff7ec669850
Suresh <javasuresh91@gmail.com>
2019-06-25 11:43:52 +0530

    Javascript Loops

---
+++ EX_05_Javascript_Basic/07_Loops/script.js
@@ -0,0 +1,10 @@
+var someVal = 10;
+for(var x =0;x<someVal;x++) {
+console.log(x);
+}
+console.log('=============================');
+var students = ["abi","suresh","vijay"];
+
+for(var x = 0 ; x <students.length; x++) {
+    console.log(students[x]);
+}
