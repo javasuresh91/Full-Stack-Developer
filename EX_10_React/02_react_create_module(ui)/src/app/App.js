5dbbb16ef191ff80b52766b7f9e7845aa67b7a8c
Suresh <javasuresh91@gmail.com>
2019-08-31 16:21:05 +0530

    Added the Product Module and appended in the Application

--- EX_10_React/02_react_create_module(ui)/src/app/App.js
+++ EX_10_React/02_react_create_module(ui)/src/app/App.js
@@ -3,7 +3,8 @@ import React, {
 } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HttpService from '../services/http-services'
+import HttpService from '../services/http-services';
+import Product from '../product/product';
 
 const http = new HttpService();
 
@@ -31,21 +32,17 @@ class App extends Component {
     }
 
     render() {
-        return ( < div className = "App" >
-            <
-            div className = "App-header" >
-            <
-            img src = {
-                logo
-            }
-            className = "App-logo"
-            alt = "logo" / >
-            <
-            h2 > Welcome to React < /h2> </div > <
-            p className = "App-intro" >
-            To get started, edit < code > src / App.js < /code> and save to reload. < /
-            p > <
-            /div>
+        return ( 
+            < div className = "App" >
+                <div className = "App-header" >
+                    <img src = {logo} className = "App-logo" alt = "logo"></img>
+                    <h2 > Welcome to React < /h2> 
+                </ div > 
+                <div className="App-main">
+                    <Product/>
+                </div>        
+                
+            < /div>
         );
     }
 }
